dbdaf9796ff730e7c19d40a9356f7dd34ba08ca4
carbon14 <10398567+david-c14@users.noreply.github.com>
2018-05-18 08:39:02 +0100

    Complete implementation of TorPatch demo

--- src/TorPatch.cpp
+++ src/TorPatch.cpp
@@ -1,16 +1,20 @@
 #include "TorpedoDemo.hpp"
+#include "dsp/digital.hpp"
 #include "torpedo.hpp"
 
 struct TorPatch;
 
 struct TorPatchOutputPort : Torpedo::PatchOutputPort {
-	TorPatchOutputPort(Module *module, unsigned int portNum):Torpedo::PatchOutputPort(module, portNum) {};
+	TorPatch *tpModule;
+	TorPatchOutputPort(TorPatch *module, unsigned int portNum):Torpedo::PatchOutputPort((Module *)module, portNum) {tpModule = module;};
+	void completed() override;
 };
 
 struct TorPatchInputPort : Torpedo::PatchInputPort {
 	TorPatch *tpModule;
 	TorPatchInputPort(TorPatch *module, unsigned int portNum):Torpedo::PatchInputPort((Module *)module, portNum) {tpModule = module;};
 	void received(std::string pluginName, std::string moduleName, json_t *rootJ) override;
+	void error(unsigned int errorType) override;
 };
 
 struct TorPatch : Module  {
@@ -33,9 +37,20 @@ struct TorPatch : Module  {
 	enum LightIds {
 		LIGHT_GREEN,
 		LIGHT_RED,
+		LIGHT_COMPLETE,
+		LIGHT_ERROR,
+		LIGHT_RECEIVE,
 		NUM_LIGHTS
 	};
+	float v1;
+	float v2;
+	float v3;
+	int toSend = 1;
 	int isDirty = 0;
+	int hasWidget = 0;
+	PulseGenerator complete;
+	PulseGenerator error;
+	PulseGenerator receive;
 	TorPatchOutputPort outPort = TorPatchOutputPort(this, OUTPUT_TOR);
 	TorPatchInputPort inPort = TorPatchInputPort(this, INPUT_TOR);
 	TorPatch() : Module(NUM_PARAMS, NUM_INPUTS, NUM_OUTPUTS, NUM_LIGHTS) {}
@@ -43,16 +58,28 @@ struct TorPatch : Module  {
 };
 
 void TorPatch::step() {
+	if (outputs[OUTPUT_V1].value != params[PARAM_1].value)
+		toSend = 1;
 	outputs[OUTPUT_V1].value = params[PARAM_1].value;
+	if (outputs[OUTPUT_V2].value != params[PARAM_2].value)
+		toSend = 1;
 	outputs[OUTPUT_V2].value = params[PARAM_2].value;
+	if (lights[LIGHT_RED].value != (params[PARAM_3].value > 0.5f))
+		toSend = 1;
 	lights[LIGHT_RED].value = (params[PARAM_3].value > 0.5f);
+	if (lights[LIGHT_GREEN].value != (params[PARAM_3].value < 1.5f))
+		toSend = 1;
 	lights[LIGHT_GREEN].value = (params[PARAM_3].value < 1.5f);
-	if (!outPort.isBusy()) {
+	lights[LIGHT_RECEIVE].value = receive.process(engineGetSampleTime());
+	lights[LIGHT_ERROR].value = error.process(engineGetSampleTime());
+	lights[LIGHT_COMPLETE].value = complete.process(engineGetSampleTime());
+	if (toSend && !outPort.isBusy()) {
 		json_t *rootJ = json_object();
 		json_object_set_new(rootJ, "param1", json_real(params[PARAM_1].value));
 		json_object_set_new(rootJ, "param2", json_real(params[PARAM_2].value));
 		json_object_set_new(rootJ, "param3", json_real(params[PARAM_3].value));
 		outPort.send(std::string(TOSTRING(SLUG)), std::string("TorPatch"), rootJ);
+		toSend = 0;
 	}
 	outPort.process();
 	inPort.process();
@@ -61,23 +88,38 @@ void TorPatch::step() {
 void TorPatchInputPort::received(std::string pluginName, std::string moduleName, json_t *rootJ) {
 	if (pluginName.compare(TOSTRING(SLUG))) return;
 	if (moduleName.compare("TorPatch")) return;
+	tpModule->receive.trigger(0.1f);
 	json_t *j1 = json_object_get(rootJ, "param1");
+
 	if (j1)
-		engineSetParam(_module, TorPatch::PARAM_1, json_number_value(j1));
+		tpModule->v1 = json_number_value(j1);
 	json_t *j2 = json_object_get(rootJ, "param2");
 	if (j2)
-		engineSetParam(_module, TorPatch::PARAM_2, json_number_value(j2));
+		tpModule->v2 = json_number_value(j2);
 	json_t *j3 = json_object_get(rootJ, "param3");
-	if (j3)
-		engineSetParam(_module, TorPatch::PARAM_3, json_number_value(j3));
-	tpModule->isDirty = true;
+	if (j3) 
+		tpModule->v3 = json_number_value(j3);
+	tpModule->isDirty = 1;
+	if (!tpModule->hasWidget) {
+		engineSetParam(tpModule, TorPatch::PARAM_1, tpModule->v1);
+		engineSetParam(tpModule, TorPatch::PARAM_2, tpModule->v2);
+		engineSetParam(tpModule, TorPatch::PARAM_3, tpModule->v3);
+	}
+}
+
+void TorPatchInputPort::error(unsigned int errorType) {
+	tpModule->error.trigger(0.1f);
+}
+
+void TorPatchOutputPort::completed() {
+	tpModule->complete.trigger(0.1f);
 }
 
 struct TorPatchWidget : ModuleWidget {
+	TorPatch *tpModule;
 	ParamWidget *p1;
 	ParamWidget *p2;
 	ParamWidget *p3;
-	TorPatch *tpModule;
 	TorPatchWidget(TorPatch *module) : ModuleWidget(module) {
 		tpModule = module;
 		setPanel(SVG::load(assetPlugin(plugin, "res/TorPatch.svg")));
@@ -97,14 +139,19 @@ struct TorPatchWidget : ModuleWidget {
 		addParam(p3);
 
 		addChild(ModuleLightWidget::create<LargeLight<GreenRedLight>>(Vec(53, 140), module, TorPatch::LIGHT_GREEN));
+		addChild(ModuleLightWidget::create<TinyLight<GreenLight>>(Vec(4, 45), module, TorPatch::LIGHT_RECEIVE));
+		addChild(ModuleLightWidget::create<TinyLight<RedLight>>(Vec(26, 45), module, TorPatch::LIGHT_ERROR));
+		addChild(ModuleLightWidget::create<TinyLight<GreenLight>>(Vec(114, 45), module, TorPatch::LIGHT_COMPLETE));
+		tpModule->hasWidget = 1;
 	}
 	void step() override {
 		if (tpModule->isDirty) {
-			p1->setValue(tpModule->params[TorPatch::PARAM_1].value);	
-			p2->setValue(tpModule->params[TorPatch::PARAM_2].value);	
-			p3->setValue(tpModule->params[TorPatch::PARAM_3].value);	
+			p1->setValue(tpModule->v1);
+			p2->setValue(tpModule->v2);
+			p3->setValue(tpModule->v3);
 			tpModule->isDirty = 0;
 		}
+		ModuleWidget::step();
 	}
 };
 
