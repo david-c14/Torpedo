df476affa2670ed24487dca1037ebf421f003f26
david-c14 <10398567+david-c14@users.noreply.github.com>
2018-06-13 14:37:52 +0100

    Completed TorNotes demo

--- src/TorNotes.cpp
+++ src/TorNotes.cpp
@@ -6,6 +6,7 @@ struct TorNotes;
 struct TorNotesInput : Torpedo::PatchInputPort {
 	TorNotes *tnModule;
 	TorNotesInput(TorNotes *module, unsigned int portNum) : Torpedo::PatchInputPort((Module *)module, portNum) { tnModule = module; }
+	void received(std::string pluginName, std::string moduleName, json_t *rootJ) override;
 };
 
 struct TorNotes : Module {
@@ -24,27 +25,39 @@ struct TorNotes : Module {
 
 		outPort.send("TorpedoDemo", "TorNotesText", rootJ); 
 	}
+	std::string text;
+	int isDirty = false;
 };
 
 struct TorNotesText : LedDisplayTextField {
 	TorNotes *tnModule;
-	void onText(EventText &e) override {
-		LedDisplayTextField::onText(e);
+	void onTextChange() override {
+		LedDisplayTextField::onTextChange();
 		tnModule->sendText(text);
 	}
 };
+
+void TorNotesInput::received(std::string pluginName, std::string moduleName, json_t *rootJ) {
+	if (pluginName.compare("TorpedoDemo")) return;
+	if (moduleName.compare("TorNotesText")) return;
+	json_t *text = json_object_get(rootJ, "text");
+	if (text) {
+		tnModule->text.assign(json_string_value(text));
+		tnModule->isDirty = true;
+	}
+}
 
 struct TorNotesWidget : ModuleWidget {
 	TorNotesText *textField;
 
 	TorNotesWidget(TorNotes *module) : ModuleWidget(module) {
-		setPanel(SVG::load(assetGlobal("res/Core/Notes.svg")));
+		setPanel(SVG::load(assetPlugin(plugin, "res/TorNotes.svg")));
 
 		addInput(Port::create<sub_port_black>(Vec(4,19), Port::INPUT, module, 0));
 		addOutput(Port::create<sub_port_black>(Vec(211,19), Port::OUTPUT, module, 0));	
 
-		textField = Widget::create<TorNotesText>(mm2px(Vec(3.39962, 14.8373)));
-		textField->box.size = mm2px(Vec(74.480, 102.753));
+		textField = Widget::create<TorNotesText>(mm2px(Vec(3.39962, 15.8373)));
+		textField->box.size = mm2px(Vec(74.480, 100.753));
 		textField->multiline = true;
 		textField->tnModule = module;
 		addChild(textField);
@@ -66,6 +79,15 @@ struct TorNotesWidget : ModuleWidget {
 		json_t *textJ = json_object_get(rootJ, "text");
 		if (textJ)
 			textField->text = json_string_value(textJ);
+	}
+
+	void step() override {
+		TorNotes *tnModule = dynamic_cast<TorNotes *>(module);
+		if (tnModule->isDirty) {
+			textField->text = tnModule->text;
+			tnModule->isDirty = false;
+		}
+		ModuleWidget::step();
 	}
 };
 
